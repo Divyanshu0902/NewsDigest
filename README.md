0ae21490afe44f30db69248fd1342373cdc37505
Divyanshu0902 <divyanshu.kumar.it28@heritageit.edu>
2026-07-01 16:09:46 +0530

    environment variable configurations added

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple application to get news from the internet from various sources,
+and send it to telegram using a telegram bot.
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-console.log("================================");
-console.log(" NewsDigest");
-console.log(" Version: V(-1)");
-console.log("================================");
-console.log("Application Started!");
+// To load environment variables from .env into the process.env object in the process of the program
+require("dotenv").config();
+
+// retrieve those environment variables from the process.env obj to seperate variables. 
+const botToken = process.env.BOT_TOKEN;
+const chatID = process.env.CHAT_ID;
+
+// printing them --
+console.log("========CONFIGURATION LOADED========");
+console.log("Bot Token:",botToken.substring(0,20)+"...");
+console.log("Chat ID:",chatID);
