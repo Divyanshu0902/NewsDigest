1d518aa9bddaa48e930309ea77087434f2f69c74
Divyanshu0902 <divyanshu.kumar.it28@heritageit.edu>
2026-07-01 14:25:13 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("================================");
+console.log(" NewsDigest");
+console.log(" Version: V(-1)");
+console.log("================================");
+console.log("Application Started!");
