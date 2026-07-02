61cccf92db7188a10e23c0095b35becb62fea762
Divyanshu0902 <divyanshu.kumar.it28@heritageit.edu>
2026-07-02 18:13:50 +0530

    Version 2 working - i.e My program is fetching top5 news from HackerNews and sending it to my telegram

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ require("dotenv").config();
 const { sendTelegramMessage } = require("./telegram");
 const { getTopStoryIDs, getStoryByID, getTopNStories} = require("./hackernews");
 
-const message = "Hello from Node.js!";
+// const message = "Hello from Node.js!";
 
 async function main() {
     const Nstories = await getTopNStories(5);
-    console.log(Nstories);
+    // const message = JSON.stringify(Nstories);
+
+    
+        const message =
+        `🔥 *Top ${Nstories.length} Hacker News Stories*\n\n` +
+        Nstories.map((story, index) => {
+            const articleLink = story.url || `https://news.ycombinator.com/item?id=${story.id}`;
+
+            return `${index + 1}. 📰 *${story.title}*
+
+        👤 Author: ${story.by}
+        ⭐ Score: ${story.score}
+        💬 Comments: ${story.descendants}
+
+        🔗 ${articleLink}
+        🗨️ https://news.ycombinator.com/item?id=${story.id}`;
+        }).join("\n\n━━━━━━━━━━━━━━━━━━━━━━\n\n");
+
+
+    // console.log(Nstories);
 
     // const StoryIDs = await getTopStoryIDs();
     // const firstStory = await getStoryByID(StoryIDs[0]);
     // console.log(firstStory);
 
-    // const data = await sendTelegramMessage(message);
-    // console.log(data);
+    const data = await sendTelegramMessage(message);
+    console.log(data);
 }
 
 main();
+
+setInterval
